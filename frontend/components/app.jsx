64e80dbe7132a25e646ef89141604b470512a60b
HelenEdwards <hgedwards87@gmail.com>
2022-01-21 16:22:34 -0800

    fix to static pages controller

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
+
 // import { Provider } from 'react-redux';
 
 const App = () => (
